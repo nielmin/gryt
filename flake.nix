f8e7e79a00941f7278d5322af764e5696314c7ca
Daniel Hwang <dawh@ines.home>
2026-02-06 04:23:26 -0600

    Add devshell flake

--- flake.nix
+++ flake.nix
@@ -2,17 +2,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;}
     {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
       systems = ["x86_64-linux"];
       perSystem = {pkgs, ...}: {
-        devShells.default = pkgs.mkShell {
+        devshells.default = {
           packages = with pkgs; [
             eza
-            go 
+            go
             gotools
             gofumpt
           ];
